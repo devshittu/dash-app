c9e2ec3b57e98dcd38b015c501327ff6fd9a6234
Muhammed Shittu <devshittu@gmail.com>
2024-02-17 00:00:00 +0000

    Configure images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.mediaapp.local',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyimage.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'xsgames.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kitwind.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  serverRuntimeConfig: {
+    apiUrl: process.env.NEXT_SERVER_API_URL,
+  },
+  publicRuntimeConfig: {
+    apiUrl: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
 export default nextConfig;
